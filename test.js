f27a84c38610c0f8ee36af1dd13cf7ad81d2a821
LestaD <lestad@bk.ru>
2015-04-26 23:21:58 +0300

    Update test

--- test.js
+++ test.js
@@ -7,7 +7,8 @@ var __c = require('./index')('test');
 "Simple log".log();
 "Indented text".li();
 
-
-
 "Run %s".log("Forrest");
 "Test with %s arguments from %s %s".debug('many', 'log', 'function');
+
+"Start log in %s environment".log("debug");
+"%s listening on %s port".info("Express", "1240");
